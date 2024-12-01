03ee466fa70953583a206fb69054a406894cc95b
Adam Magnusson <admag2001@gmail.com>
2024-12-01 14:26:38 +0100

    Day 1 part 2

--- aoc24/lib/day1.ex
+++ aoc24/lib/day1.ex
@@ -1,28 +1,20 @@
 defmodule Day1 do
 
   def main do
-    pattern = :binary.compile_pattern([" ", "\n"])
-    input = File.read!("inputs/day1.inp") |> String.split(pattern) |> List.delete_at(-1) |> IO.inspect() 
-    %{true => left, false => right} = make_heaps(%{true => Heap.new, false => Heap.new}, input, true)
-    sum = diff_sum(left, right, 0)
-  end
+    {left, right} = File.read!("inputs/day1.inp") 
+    |> String.split("\n")
+    |> List.delete_at(-1)
+    |> Enum.reduce({[], []}, fn <<l::binary-size(5), " ", r::binary-size(5)>>, {left, right} -> {[String.to_integer(l) | left], [String.to_integer(r) | right]} end)
 
-  def diff_sum(_left, %Heap{data: nil}, sum) do
-    sum
-  end
+    Enum.reduce(left, 0, fn l, score ->
+      score + Enum.reduce(right, 0, fn r, sum -> 
+        if l == r do
+          sum + r 
+        else
+          sum
+        end 
+      end)      
+    end)
 
-  def diff_sum(left, right, sum) do
-    {left_num, right_num} = {Heap.root(left), Heap.root(right)}
-    diff = abs(left_num - right_num)
-    diff_sum(Heap.pop(left), Heap.pop(right), sum + diff)
-  end
-
-  def make_heaps(heaps, [], _left) do
-    heaps
-  end
-
-  def make_heaps(heaps, [num | input], left) do
-    new_heap = heaps[left] |> Heap.push(String.to_integer(num))
-    make_heaps(%{heaps | left => new_heap}, input, not left)
   end
 end
